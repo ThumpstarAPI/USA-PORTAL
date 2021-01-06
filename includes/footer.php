16a2db61a3656ce83c25ec72816b370d2762f20c
ThumpstarAPI <2133069@slu.edu.ph>
2021-01-06 18:49:44 +0800

    includes folder

---
+++ includes/footer.php
@@ -0,0 +1,128 @@
+<section class="logo-section py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-3">
+                <img class="img-fluid mx-auto d-block" src="https://www.thumpstar.com.au/files/images/pipebomb-exhaust.png" alt="Pipebomb Exhaust" />
+            </div>
+            <div class="col-md-4 mb-3">
+                <img class="img-fluid mx-auto d-block" src="https://www.thumpstar.com.au/files/images/rev-king.png" alt="Rev King" />
+            </div>
+            <div class="col-md-4 mb-3">
+                <img class="img-fluid mx-auto d-block" src="https://www.thumpstar.com.au/files/images/vs1.png" alt="VS1" />
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer class="py-md-5 pt-3 text-left">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 pb-3 pb-lg-0">
+                <h3 class="text-uppercase py-3 m-0">Quick Links</h3>
+                <ul class="m-0 p-0">
+                    <li class="pb-2"><a class="d-block" href="https://www.thumpstar.com.au/database/">Database</a></li>
+                    <li class="pb-2"><a class="d-block" href="https://www.thumpstar.com.au/laybuy/">Laybuy</a></li>
+                    <li class="pb-2"><a class="d-block" href="https://www.thumpstar.com.au/gallery/">Gallery</a></li>
+                    <li class="pb-2"><a class="d-block" href="https://www.thumpstar.com.au/shipping/">Shipping</a></li>
+                    <li class="pb-2"><a class="d-block" href="https://www.thumpstar.com.au/warranty/">Warranty</a></li>
+                    <li class="pb-2"><a class="d-block" href="https://www.thumpstar.com.au/workshop/">Workshop</a></li>
+                    <li class="pb-2"><a class="d-block" href="https://www.thumpstar.com.au/credit-card/">Credit Card / Paypal</a></li>
+                    <li class="pb-2"><a class="d-block" href="https://www.thumpstar.com.au/testimonials/">Testimonials</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 pb-3 pb-lg-0">
+                <h3 class="text-uppercase py-3 m-0">Contact Us</h3>
+                <div class="d-flex align-items-center pb-3">
+                    <div class="pr-3">
+                        <i class="fas fa-map-marker-alt"></i>
+                    </div>
+                    <div>
+                        <a href="https://www.thumpstar.com.au/dealer-locator/">Dealer Locator</a>
+                        <p class="m-0">Thumpstar Australia</p>
+                        <p class="m-0">Dealer No.<strong>26768</strong></p>
+                    </div>  
+                </div>
+                <div class="d-flex align-items-center pb-3">
+                    <div class="pr-3">
+                        <i class="fas fa-phone-alt"></i>
+                    </div>
+                    <div>
+                        <a href="tel:0393441564">03 9344 1564</a>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center pb-3">
+                    <div class="pr-3">
+                        <i class="fas fa-envelope"></i>
+                    </div>
+                    <div>
+                        <a href="mailto:support@thumpstar.com.au">support@thumpstar.com.au</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 pb-3 pb-lg-0">
+                <h3 class="text-uppercase py-3 m-0">Payments</h3>
+                <div class="row no-gutter">
+                    <div class="col-12">
+                        <a href="https://www.thumpstar.com.au/credit-card/">
+                            <img class="img-fluid" src="https://www.thumpstar.com.au/files/images/payment.png" alt="Thumpstar Payment Method" />
+                        </a>
+                    </div>
+                    <div class="col-12 payment-images">
+                        <a class="d-block w-100" href="https://www.thumpstar.com.au/laybuy/">
+                            <img class="img-fluid" src="https://www.thumpstar.com.au/files/images/laybuy.jpg" alt="Thumpstar Laybuy Method" />
+                        </a>
+                        <a class="d-block w-100" href="https://www.thumpstar.com.au/afterpay/">
+                            <img class="img-fluid" src="https://www.thumpstar.com.au/files/images/afterpay.jpg" alt="Thumpstar Afterpay Method" />
+                        </a>
+                        <a class="d-block w-100" href="https://www.walletwizard.com.au/apply-now" target="_blank">
+                            <img class="img-fluid" src="https://www.thumpstar.com.au/files/images/wallet-wizard.jpg" alt="Thumpstar Wallet Wizard Method" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 pb-3 pb-lg-0">
+                <h3 class="text-uppercase py-3 m-0">Follow Us</h3>
+                <div class="d-flex align-items-center pb-3">
+                    <i class="fab fa-facebook-square"></i>
+                    <a class="d-block pl-2" href="https://www.facebook.com/thumpstarmoto" target="_blank">/thumpstarmoto</a>
+                </div>
+                <div class="d-flex align-items-center pb-3">
+                    <i class="fab fa-twitter-square"></i>
+                    <a class="d-block pl-2" href="https://twitter.com/thumpstarmoto" target="_blank">/thumpstarmoto</a>
+                </div>
+                <div class="d-flex align-items-center pb-3">
+                    <i class="fab fa-instagram"></i>
+                    <a class="d-block pl-2" href="https://www.instagram.com/genuinethumpstar/" target="_blank">/genuinethumpstar</a>
+                </div>
+                <div class="d-flex align-items-center pb-3">
+                    <i class="fab fa-youtube-square"></i>
+                    <a class="d-block pl-2" href="https://www.youtube.com/user/reaperbikes" target="_blank">/thumpstar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<section class="copyright py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <ul class="list-group list-group-horizontal text-center d-block d-lg-flex">
+                  <li class="pr-lg-3 pr-0"><a href="https://www.thumpstar.com.au/terms-conditions/">Terms & Conditions</a></li>
+                  <li class="pr-lg-3 pr-0"><a href="https://www.thumpstar.com.au/licensing/">Licensing</a></li>
+                  <li class="pr-lg-3 pr-0"><a href="https://www.thumpstar.com.au/privacy-policy/">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-6">
+                <p class="m-0 text-center text-lg-right">Copyright &copy; <?php echo date("Y"); ?> Thumpstar Australia</p>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="https://www.thumpstar.com.au/resources/js/menumaker-min.js"></script>
+<script type="text/javascript">
+	$("#cssmenu").menumaker({
+		format: "multitoggle"
+	});
+</script>
+</body>
+</html>
